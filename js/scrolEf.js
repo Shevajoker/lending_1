01e0f3c67648e2e564f2c17065f4c9d9b4b3e081
Sheva <Sheva@mail.com>
2016-12-07 16:26:03 +0300

    scroll efect_2

--- js/scrolEf.js
+++ js/scrolEf.js
@@ -1,9 +1,9 @@
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
     $(".zoom img").css({
-        transform: 'translate3d(-50%, -'+(scroll/150)+'%, 0) scale('+(150 + scroll/5)/150+')',
-        //Blur suggestion from @janwagner: http://codepen.io/janwagner/ in comments
-        //"-webkit-filter": "blur(" + (scroll/200) + "px)",
-        //filter: "blur(" + (scroll/200) + "px)"
+        transform: 'translate3d(-50%, -'+(scroll/150)+'%, 0) scale('+(150 + scroll/80)/150+')',
+//        Blur suggestion from @janwagner: http://codepen.io/janwagner/ in comments
+//        "-webkit-filter": "blur(" + (scroll/200) + "px)",
+//        filter: "blur(" + (scroll/200) + "px)"
     });
 });
